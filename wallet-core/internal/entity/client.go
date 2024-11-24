bd2fcbcd8f228a4871eabf5cad31c5502307654d
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2024-11-24 20:02:50 -0300

    feat: created client entity ms wallet-core

---
+++ wallet-core/internal/entity/client.go
@@ -0,0 +1,43 @@
+package entity
+
+import (
+	"errors"
+	"github.com/google/uuid"
+
+	"time"
+)
+
+type Client struct {
+	ID        string
+	Name      string
+	Email     string
+	CreatedAt string
+	UpdatedAt string
+}
+
+func NewClient(name string, email string) (*Client, error) {
+	client := &Client{
+		ID:        uuid.New().String(),
+		Name:      name,
+		Email:     email,
+		CreatedAt: time.Now().String(),
+		UpdatedAt: time.Now().String(),
+	}
+
+	if err := client.Validate(); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (c *Client) Validate() error {
+	if c.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if c.Email == "" {
+		return errors.New("email is required")
+	}
+	return nil
+}
---
+++ wallet-core/internal/entity/client_test.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateClient(t *testing.T) {
+	client, err := NewClient("John Doe", "j@j.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "John Doe", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
