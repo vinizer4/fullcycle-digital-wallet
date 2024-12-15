e57ccbda9993f53fbd729c299f4532ed14a376cb
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2024-12-15 15:11:05 -0300

    feat: created integration tests to account database client

--- wallet-core/internal/database/account_db_test.go
+++ wallet-core/internal/database/account_db_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com.br/devfullcycle/fc-ms-wallet/wallet-core/internal/entity"
 	"github.com/stretchr/testify/suite"
 	"testing"
+	"time"
 )
 
 type AccountDBTestSuite struct {
@@ -33,4 +34,36 @@ func (s *AccountDBTestSuite) TearDownSuite() {
 
 func TestAccountDBTestSuite(t *testing.T) {
 	suite.Run(t, new(AccountDBTestSuite))
+}
+
+func (s *AccountDBTestSuite) TestSave() {
+	account := entity.NewAccount(s.client)
+	err := s.AccountDB.Save(account)
+	s.Nil(err)
+}
+
+func (s *AccountDBTestSuite) TestFindByID() {
+	// Given
+	s.db.Exec("insert into clients (id, name, email, created_at, updated_at) values (?, ?, ?, ?, ?)",
+		s.client.ID, s.client.Name, s.client.Email, s.client.CreatedAt, s.client.UpdatedAt,
+	)
+
+	account := entity.NewAccount(s.client)
+	err := s.AccountDB.Save(account)
+	s.Nil(err)
+
+	// When
+	accountDB, err := s.AccountDB.FindByID(account.ID)
+
+	// Then
+	s.Nil(err)
+	s.Equal(account.ID, accountDB.ID)
+	s.Equal(account.Client.ID, accountDB.Client.ID)
+	s.Equal(account.Balance, accountDB.Balance)
+	s.Equal(account.CreatedAt.In(time.Local), accountDB.CreatedAt.In(time.Local))
+	s.Equal(account.UpdatedAt.In(time.Local), accountDB.UpdatedAt.In(time.Local))
+	s.Equal(account.Client.Name, accountDB.Client.Name)
+	s.Equal(account.Client.Email, accountDB.Client.Email)
+	s.Equal(account.Client.CreatedAt.In(time.Local), accountDB.Client.CreatedAt.In(time.Local))
+	s.Equal(account.Client.UpdatedAt.In(time.Local), accountDB.Client.UpdatedAt.In(time.Local))
 }
